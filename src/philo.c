0591314e31d32983bcbe024f2183ebc469d0a849
Nonaudh <nonaudh@gmail.com>
2024-05-02 00:54:05 +0200

    ok

--- src/philo.c
+++ src/philo.c
@@ -1,6 +1,6 @@
 #include "../inc/philo.h"
 
-void	eating(t_philo *p)
+void	eating_right_first(t_philo *p)
 {
 	pthread_mutex_lock(&p->right_fork);
 	printf("%d right fork\n", p->number);
@@ -13,6 +13,22 @@ void	eating(t_philo *p)
 	pthread_mutex_unlock(p->left_fork);
 	printf("%d left drop\n", p->number);
 }
+
+void	eating_left_first(t_philo *p)
+{
+	pthread_mutex_lock(p->left_fork);
+	printf("%d left fork\n", p->number);
+	pthread_mutex_lock(&p->right_fork);
+	printf("%d right fork\n", p->number);
+	printf("%d eating\n", p->number);
+	usleep(p->time_to_eat);
+	pthread_mutex_unlock(p->left_fork);
+	printf("%d left drop\n", p->number);
+	pthread_mutex_unlock(&p->right_fork);
+	printf("%d right drop\n", p->number);
+}
+	
+
 void	sleeping(t_philo *p)
 {
 	printf("%d sleeping\n", p->number);
@@ -25,16 +41,18 @@ void	thinking(t_philo *p)
 
 void	*routine(void *p)
 {
-	//t_philo *tmp;
+	t_philo *tmp;
 
-	//tmp = p;
+	tmp = p;
 	int i = 0;
 	while (i < 3)//!p->stop)
 	{
-		//if (tmp->number == 1)
-			eating(p);
-		sleeping(p);
-		thinking(p);
+		if (tmp->number % 2 == 0)
+			eating_right_first(tmp);
+		else
+			eating_left_first(tmp);
+		sleeping(tmp);
+		thinking(tmp);
 		i++;
 	}
 	return (NULL);
