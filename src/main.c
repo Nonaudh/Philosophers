f29f8a0d6d28dcad1eab228957cf40cbef2b916b
Nonaudh <nonaudh@gmail.com>
2024-05-03 11:14:14 +0200

    ok

--- src/main.c
+++ src/main.c
@@ -1,35 +1,35 @@
 #include "../inc/philo.h"
 void	right_first(t_philo *p)
 {
-	pthread_mutex_lock(&p->right_fork);
-	printf("%d lock_right\n", p->id);
-	pthread_mutex_lock(p->left_fork);
-	printf("%d lock_left\n", p->id);
-	printf("%d eat\n", p->id);
-	usleep(500);
-	pthread_mutex_unlock(&p->right_fork);
-	printf("%d unlock_right\n", p->id);
-	pthread_mutex_unlock(p->left_fork);
-	printf("%d unlock_left\n", p->id);
+	pthread_mutex_lock(&p[0].right_fork);
+	//printf("%d lock_right\n", p->id);
+	pthread_mutex_lock(p[0].left_fork);
+	//printf("%d lock_left\n", p->id);
+	//printf("%d eat\n", p->id);
+	usleep(5000);
+	pthread_mutex_unlock(p[0].left_fork);
+	//printf("%d unlock_left\n", p->id);
+	pthread_mutex_unlock(&p[0].right_fork);
+	//printf("%d unlock_right\n", p->id);
 }
 void	left_first(t_philo *p)
 {
-	pthread_mutex_lock(p->left_fork);
-	printf("%d lock_left\n", p->id);
-	pthread_mutex_lock(&p->right_fork);
-	printf("%d lock_right\n", p->id);
-	printf("%d eat\n", p->id);
-	usleep(500);
-	pthread_mutex_unlock(p->left_fork);
-	printf("%d unlock_left\n", p->id);
-	pthread_mutex_unlock(&p->right_fork);
-	printf("%d unlock_right\n", p->id);
+	pthread_mutex_lock(p[0].left_fork);
+	//printf("%d lock_left\n", p->id);
+	pthread_mutex_lock(&p[0].right_fork);
+	//printf("%d lock_right\n", p->id);
+	//printf("%d eat\n", p->id);
+	usleep(5000);
+	pthread_mutex_unlock(&p[0].right_fork);
+	//printf("%d unlock_right\n", p->id);
+	pthread_mutex_unlock(p[0].left_fork);
+	//printf("%d unlock_left\n", p->id);
 }
 
 
 void	eating(t_philo *p)
 {
-	if (p->id % 2 == 0)
+	if (p[0].id % 2 == 0)
 		right_first(p);
 	else
 		left_first(p);
@@ -37,13 +37,13 @@ void	eating(t_philo *p)
 
 void	sleeping(t_philo *p)
 {
-	printf("%d sleep\n", p->id);
+	//printf("%d sleep\n", p->id);
 	usleep(200);
 }
 
 void	thinking(t_philo *p)
 {
-	printf("%d think\n", p->id);
+	//printf("%d think\n", p->id);
 }
 
 void	*routine(void *p)
@@ -58,6 +58,7 @@ void	*routine(void *p)
 		thinking(tmp);
 		i++;
 	}
+	return (NULL);
 }
 
 void	philo(t_philo *p, pthread_t *t, int number)
@@ -67,6 +68,7 @@ void	philo(t_philo *p, pthread_t *t, int number)
 	while (i < number)
 	{
 		pthread_create(&t[i], NULL, routine, &p[i]);
+		usleep(50);
 		i++;
 	}
 }
